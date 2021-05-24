c1291d00318b665017dc934f795e59eece9c8830
TongDaxia <tong_y_g@163.com>
2021-05-24 14:01:16 +0800

    还真不能乱加东西啊，想修改源码都这么难么

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -5,6 +5,8 @@ import org.apache.commons.logging.LogFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @MapperScan("com.kaoshi.tyg.mapper")
